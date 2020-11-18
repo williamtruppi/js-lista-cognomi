5fce0a699cbaa1e5864b3947b666e59921ad0af4
William Truppi <williamt@hotmail.it>
2020-11-18 15:49:20 +0100

    stampa su html dell'indice di posizionamento con indexof e confronto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
-//creazione dell'array
-var surnameList = ["Pino", "Siffredi", "Albertini", "Zazza", "Cristiano"];
+//creazione dell'array con output su html
+var surnameList = [
+  "Pino", 
+  "Siffredi", 
+  "Albertini", 
+  "Zazza", 
+  "Cristiano"
+];
 console.log(surnameList);
+var outputArray = document.getElementById("first_array");
+outputArray.innerHTML = "Array appena generato ---> " + surnameList;
 
 //acquisizione cognome utente
 var surnameUser = prompt("Inserire il proprio cognome");
@@ -9,15 +17,28 @@ console.log(surnameUser);
 //inserimento del cognome dell'utente nell'array
 surnameList.push(surnameUser);
 console.log(surnameList);
+var outputArray = document.getElementById("before_sort");
+outputArray.innerHTML = "Array NON ordinato con cognome utente  ---> " + surnameList;
 
 //ordinamento alfabetico dell'array
 surnameList.sort();
 console.log(surnameList);
+var outputArray = document.getElementById("after_sort");
+outputArray.innerHTML = "Array ORDINATO con cognome utente  ---> " + surnameList;
 
+
+var messageIndexComparison = document.getElementById("comparison_index");
+
+//comparazione dell'index e successiva stampa
 i = 0;
 while (i < surnameList.length){
   if(surnameList[i] == surnameUser){
     console.log("Il tuo cognome, dopo l'ordinamento, è in posizione " + i);
+    messageIndexComparison.innerHTML = " PRIMA SOLUZIONE CON CONFRONTO DELL'INDEX --- Il tuo cognome, dopo l'ordinamento, è in posizione " + i;
   }
   i++;
 }
+
+//utlizzo metodo indexof
+var messageIndexOf = document.getElementById("indexof_method");
+messageIndexOf.innerHTML = "SECONDA SOLUZIONE CON METODO INDEXOF --- Il tuo cognome, dopo l'ordinamento, è in posizione " + surnameList.indexOf(surnameUser);
