dea2fe1d5e6737e30f3d5b86adba826820f4fcbe
William Truppi <williamt@hotmail.it>
2020-11-18 14:50:31 +0100

    cognome acquisito in input ed inserito nell'array

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//creazione dell'array
+var surnameList = ["Pino", "Siffredi", "Albertini", "Zazza", "Cristiano"];
+console.log(surnameList);
+
+//acquisizione cognome utente
+var surnameUser = prompt("Inserire il proprio cognome");
+console.log(surnameUser);
+
+//inserimento del cognome dell'utente nell'array
+surnameList.push(surnameUser);
+console.log(surnameList);
